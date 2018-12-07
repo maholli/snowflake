8e18feef3192aec21f73475dbcfdaa40fe9c3f89
Max Holliday <maholli@stanford.edu>
2018-12-06 22:18:23 -0800

    added readme and other firmware stuff

--- firmware/main.py
+++ firmware/main.py
@@ -1,18 +1,19 @@
-import microcontroller
-import neopixel, time
+import board
+import neopixel
+import touchio
+import time
 
-WHITE = (50, 50, 50)
-OFF   = (0,   0,  0)
+# Initalize the capacitive touch buttons
+# They are the two pointy bits opposite from the USB connector.
+touch0 = touchio.TouchIn(board.CAP0)
+touch1 = touchio.TouchIn(board.CAP1)
 
-pixels = neopixel.NeoPixel(microcontroller.pin.PA17, 15)
-for i in range(15):
-    pixels[i] = WHITE
-pixels.show()
+# Initialize the LEDS. There are 15 of them.
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 15)
 
-
-def wheel(pos):
+def wheel(pos): # A function to make pretty colors.
     # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
+    # The colors are a transition r - g - b - back to r.
     if (pos < 0) or (pos > 255):
         return (0, 0, 0)
     if pos < 85:
@@ -25,11 +26,29 @@ def wheel(pos):
     return (int(pos*3), 0, int(255 - pos*3))
 
 pixeln = 0
-pixels.brightness = 0.3
+pixels.brightness = 0.6
+
+# MAIN LOOP. Runs forever.
 while True:
+	# check if the buttons are pressed
+    while touch0.value:
+        print("CAP0 touched!")
+        for i in range(15):
+    		pixels[i] = (250, 250, 250) # WHITE
+    	time.sleep(0.1)
+    while touch1.value:
+        print("CAP1 touched!")
+        for j in range(15):
+    		pixels[j] = (140, 0, 0) # CARDINAL RED
+    	time.sleep(0.1)
+    
+    #loop through all the LEDs
     for p in range(15):
         color = wheel(25 * ((pixeln + p)%15))
         pixels[p] = color
     pixeln += 1
     if pixeln > 15:
         pixeln = 0
+
+    #sleep for 200 ms
+    time.sleep(0.2)
--- firmware/snowflake_v1a/pins.c
+++ firmware/snowflake_v1a/pins.c
@@ -9,8 +9,7 @@ STATIC const mp_rom_map_elem_t board_global_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_MOSI), MP_ROM_PTR(&pin_PA20) },
     { MP_ROM_QSTR(MP_QSTR_MISO), MP_ROM_PTR(&pin_PA16) },
     { MP_ROM_QSTR(MP_QSTR_CAP0), MP_ROM_PTR(&pin_PA06) },
-    { MP_ROM_QSTR(MP_QSTR_CAP1), MP_ROM_PTR(&pin_PB22) },
-    { MP_ROM_QSTR(MP_QSTR_CAP2), MP_ROM_PTR(&pin_PB03) },
+    { MP_ROM_QSTR(MP_QSTR_CAP1), MP_ROM_PTR(&pin_PB03) },
     
     { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
     
